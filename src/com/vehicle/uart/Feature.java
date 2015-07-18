359c0db3cace673938e67cc61d79229952f8f58a
liudancodeman <67712257@qq.com>
2015-07-18 21:52:54 +0800

    add SplashActivity

--- src/com/vehicle/uart/Feature.java
+++ src/com/vehicle/uart/Feature.java
@@ -5,5 +5,5 @@ public final class Feature
 	public final static boolean blEnableLog = true;
 	public final static boolean blDemo = true;
 	public final static boolean blSimulatorMode = true;
-	public final static String g_Version = "V0.1_Date_2015_07_17";
+	public final static String g_Version = "V0.1 2015_07_18";
 }
