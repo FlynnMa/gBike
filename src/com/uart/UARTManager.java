deab02558332c69e48a0ff763d666537adc49039
yunfei <shmayunfei@qq.com>
2015-07-25 10:36:47 +0800

    add profile, uart components support, add nrfgo utility API support

---
+++ src/com/uart/UARTManager.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright (c) 2015, Nordic Semiconductor
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.uart;
+
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattService;
+import android.content.Context;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.UUID;
+
+import com.profile.BleManager;
+import com.uart.UARTManagerCallbacks;
+import com.profile.BleProfileService;
+
+public class UARTManager extends BleManager<UARTManagerCallbacks> {
+	/** Nordic UART Service UUID */
+	private final static UUID UART_SERVICE_UUID = UUID.fromString("6E400001-B5A3-F393-E0A9-E50E24DCCA9E");
+	/** TX characteristic UUID */
+	private final static UUID UART_TX_CHARACTERISTIC_UUID = UUID.fromString("6E400002-B5A3-F393-E0A9-E50E24DCCA9E");
+	/** RX characteristic UUID */
+	private final static UUID UART_RX_CHARACTERISTIC_UUID = UUID.fromString("6E400003-B5A3-F393-E0A9-E50E24DCCA9E");
+
+	private BluetoothGattCharacteristic mTXCharacteristic, mRXCharacteristic;
+
+	public UARTManager(final Context context) {
+		super(context);
+	}
+
+	@Override
+	protected BleManagerGattCallback getGattCallback() {
+		return mGattCallback;
+	}
+
+	/**
+	 * BluetoothGatt callbacks for connection/disconnection, service discovery, receiving indication, etc
+	 */
+	private final BleManagerGattCallback mGattCallback = new BleManagerGattCallback() {
+
+		@Override
+		protected Queue<Request> initGatt(final BluetoothGatt gatt) {
+			final LinkedList<Request> requests = new LinkedList<>();
+			requests.push(Request.newEnableNotificationsRequest(mRXCharacteristic));
+			return requests;
+		}
+
+		@Override
+		public boolean isRequiredServiceSupported(final BluetoothGatt gatt) {
+			final BluetoothGattService service = gatt.getService(UART_SERVICE_UUID);
+			if (service != null) {
+				mTXCharacteristic = service.getCharacteristic(UART_TX_CHARACTERISTIC_UUID);
+				mRXCharacteristic = service.getCharacteristic(UART_RX_CHARACTERISTIC_UUID);
+			}
+			return mTXCharacteristic != null && mRXCharacteristic != null;
+		}
+
+		@Override
+		protected void onDeviceDisconnected() {
+			mTXCharacteristic = null;
+			mRXCharacteristic = null;
+		}
+
+		@Override
+		public void onCharacteristicWrite(final BluetoothGatt gatt, final BluetoothGattCharacteristic characteristic) {
+			final String data = characteristic.getStringValue(0);
+			mCallbacks.onDataSent(data);
+		}
+
+		@Override
+		public void onCharacteristicNotified(final BluetoothGatt gatt, final BluetoothGattCharacteristic characteristic) {
+			final String data = characteristic.getStringValue(0);
+			mCallbacks.onDataReceived(data);
+		}
+	};
+
+	@Override
+	protected boolean shouldAutoConnect() {
+		// We want the connection to be kept
+		return true;
+	}
+
+	/**
+	 * Sends the given text to TH characteristic.
+	 * @param text the text to be sent
+	 */
+	public void send(final String text) {
+		if (mTXCharacteristic != null) {
+			mTXCharacteristic.setValue(text);
+			writeCharacteristic(mTXCharacteristic);
+		}
+	}
+}
